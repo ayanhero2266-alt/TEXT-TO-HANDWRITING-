896d151a5e20c6f6919ec282dd45f03a8ed5dbc9
ayanhero2266-alt <ayanhero2266@gmail.com>
2025-12-19 00:46:54 +0530

    pu

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
 function convertText() {
-  let text = document.getElementById("inputText").value;
-  document.getElementById("outputText").innerText = text;
+      let text = document.getElementById("inputText").value;
+        let size = document.getElementById("fontSize").value;
+
+          let output = document.getElementById("outputText");
+            output.innerText = text;
+              output.style.fontSize = size + "px";
+              }
+
+              // IMAGE DOWNLOAD
+              function downloadImage() {
+                html2canvas(document.getElementById("paper")).then(canvas => {
+                    let link = document.createElement("a");
+                        link.download = "handwriting.png";
+                            link.href = canvas.toDataURL();
+                                link.click();
+                                  });
+                                  }
+
+                                  // PDF DOWNLOAD
+                                  function downloadPDF() {
+                                    html2canvas(document.getElementById("paper")).then(canvas => {
+                                        const { jsPDF } = window.jspdf;
+                                            let pdf = new jsPDF("p", "mm", "a4");
+
+                                                let imgData = canvas.toDataURL("image/png");
+                                                    let imgWidth = 210;
+                                                        let imgHeight = (canvas.height * imgWidth) / canvas.width;
+
+                                                            pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+                                                                pdf.save("handwriting.pdf");
+                                                                  });
+                                                                  }
 }
+  
 
-// IMAGE DOWNLOAD
-function downloadImage() {
-  html2canvas(document.getElementById("paper")).then(canvas => {
-    let link = document.createElement("a");
-    link.download = "handwriting.png";
-    link.href = canvas.toDataURL();
-    link.click();
-  });
-}
 
-// PDF DOWNLOAD
-function downloadPDF() {
-  html2canvas(document.getElementById("paper")).then(canvas => {
-    const { jsPDF } = window.jspdf;
-    let pdf = new jsPDF("p", "mm", "a4");
 
-    let imgData = canvas.toDataURL("image/png");
-    let imgWidth = 210;
-    let imgHeight = (canvas.height * imgWidth) / canvas.width;
+  
+ 
+   
+ 
+  
 
-    pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
-    pdf.save("handwriting.pdf");
-  });
-}
+
+
+  
+  
+
+   
+
+   
+    
+
 
